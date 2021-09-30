2fe2ea3a5c31eb625ef1f1b85ee5f84bd2d5c76e
Bar Horing Amir <bar.apple@gmail.com>
2021-09-30 23:42:07 +0300

    dev-react

--- react-nginx/src/App.js
+++ react-nginx/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hi Bar!</p>
+        <p>Hi Bar!!! Horing Amir!</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
